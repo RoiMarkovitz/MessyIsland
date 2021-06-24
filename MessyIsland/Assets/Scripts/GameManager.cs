a95607c135be26d1a0046515207771fba43a1c4e
roi-c <68189545+roi-c@users.noreply.github.com>
2021-06-24 18:24:41 +0300

    Added weapons, random weapon locations

---
+++ MessyIsland/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    const float ACTIVATED_WEAPONS_PERCENTAGE = 0.5f;
+    [SerializeField] GameObject[] grenades;
+    [SerializeField] GameObject[] pistols;
+
+
+    void Start()
+    {
+        bool[] isActivatedGrenades = new bool[grenades.Length];
+        bool[] isActivatedPistols = new bool[pistols.Length];
+
+        setActivatedWeapons(isActivatedGrenades, grenades);
+        setActivatedWeapons(isActivatedPistols, pistols);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void setActivatedWeapons(bool[] isActivatedWeaponsArray, GameObject[] weapons)
+    {
+        int numOfActivatedWeapon = (int)(isActivatedWeaponsArray.Length * ACTIVATED_WEAPONS_PERCENTAGE);
+
+        for (int i = 0; i < numOfActivatedWeapon; i++)
+        {
+            isActivatedWeaponsArray[i] = true;
+        }
+
+        shuffleWeapons(isActivatedWeaponsArray);
+
+        for (int i = 0; i < isActivatedWeaponsArray.Length; i++)
+        {
+            if (isActivatedWeaponsArray[i])
+            {
+                weapons[i].SetActive(true);
+            }
+        }
+
+    }
+
+    void shuffleWeapons(bool[] isActivatedWeaponsArray)
+    {
+        for (int i = 0; i < isActivatedWeaponsArray.Length; i++)
+        {
+            int random = Random.Range(0, isActivatedWeaponsArray.Length);
+            bool temp = isActivatedWeaponsArray[random];
+            isActivatedWeaponsArray[random] = isActivatedWeaponsArray[i];
+            isActivatedWeaponsArray[i] = temp;
+        }
+    }
+}
